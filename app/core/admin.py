00f8c24d1599855abba6438417e974816bdb6f73
Augusto Kestering Longo <augusto.kestering@gmail.com>
2021-03-27 11:12:36 -0300

    Correction for flake8

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,32 +6,33 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
+
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (None, {
-            'fields': (
-                'email',
-                'password'
-            ),
-        }),
-        (_('Personal Info'), {
-            'fields': (
-                'name',
-            )
-        }),
-        (_('Permissions'), {
-            'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser',
-            )
-        }),
-         (_('Important dates'), {
-            'fields': (
-                'last_login',
-            )
-        }),
+            (None, {
+                'fields': (
+                    'email',
+                    'password'
+                ),
+            }),
+            (_('Personal Info'), {
+                'fields': (
+                    'name',
+                )
+            }),
+            (_('Permissions'), {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }),
+            (_('Important dates'), {
+                'fields': (
+                    'last_login',
+                )
+            }),
     )
     add_fieldsets = (
         (None, {
@@ -45,5 +46,6 @@ class UserAdmin(BaseUserAdmin):
             )
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -26,7 +26,7 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.email)
 
     def test_user_change_page(self):
-        url = reverse('admin:core_user_change',args=[self.user.id])
+        url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
